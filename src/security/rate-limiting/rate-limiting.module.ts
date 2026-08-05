07d1f38b1942c61a96df185ae7cb61b2dbb9bbeb
Mouloud <m.hasrane@esi-sba.dz>
2026-08-05 16:10:14 +0100

    feat(rate): limiting

--- src/security/rate-limiting/rate-limiting.module.ts
+++ src/security/rate-limiting/rate-limiting.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigType } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import appConfig from 'src/config/app.config';
-
+import redisConfig from 'src/config/redis.config';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 @Module({
   imports: [
     ThrottlerModule.forRootAsync({
       imports: [ConfigModule.forFeature(appConfig)],
-      inject: [appConfig.KEY],
-      useFactory: (configService: ConfigType<typeof appConfig>) => ({
+      inject: [appConfig.KEY, redisConfig.KEY],
+      useFactory: (
+        configService: ConfigType<typeof appConfig>,
+        redisConf: ConfigType<typeof redisConfig>,
+      ) => ({
+        storage: new ThrottlerStorageRedisService({
+          host: redisConf.host,
+          port: redisConf.port,
+          db: 2,
+        }),
         throttlers: [
           {
             name: 'global',
